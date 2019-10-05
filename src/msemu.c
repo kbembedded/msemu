kbembedded/msemu

commit 04267bb2e13e9832946aaab94323c55834454819

msemu: Convert all NMI calls to INT calls
Also add proper return for intread callback.

It was found that the real hardware for sure implements INT, but
it is now not known when or if NMI is ever used. The external NMI
pin on the Mailstation CPU is tied high, there is no RETN call in
the firmware, but there is also careful code practices around the
0x0038 INT handler and the 0x0066 NMI handler in the stock firmware.

src/msemu.c
@@ -567,21 +567,30 @@ void generateKeyboardMatrix(MSHW* ms, int scancode, int eventtype)
 	}
 }
 
-/* z80ex emulation requires that intread callback be defined.
- * As far as is known, there is no INT used on the Mailsattion. The only IRQs
- * are NMI for timers, which launch other handlers from there. If the error
- * below is ever encountered, that means that this actually is used somewhere.
+/* z80ex emulation requires that intread callback be defined. Used for IM 2.
+ * Under normal execution, IM 2 is not used, however, some of the hacks used
+ * for installing a custom interrupt handler use IM 2. For these to work, we
+ * must provide intread capability. In theory, the return data should be fully
+ * random as in real hardware these pins all go high impedance with no pull
+ * resistors.
  */
 Z80EX_BYTE z80ex_intread (
 	Z80EX_CONTEXT *cpu,
 	void *user_data)
 {
-	log_error("z80ex_intread is not implemented.");
-	abort();
+	return 0x00;
 }
 
-// Processes interrupts. Should be called on every interrupt period.
-// Returns number of tstates spent processing interrupt.
+/* Processes interrupts. Should be called on every interrupt period.
+ * Returns number of tstates spent processing interrupt.
+ *
+ * NOTE: It is unknown if an NMI ever occurs on the Mailstation at this time.
+ * Disassembly of the firmware yields no calls to RETN, however the code at
+ * 0x0066 is set up to handle an NMI. That is, 0x0065 is a NOP since the opcode
+ * at 0x0066 is the start of a jump. If the jump were to start at 0x0065 then
+ * 0x0066 would not have a valid instruction. This hints that there is some
+ * expectation of an NMI occurring.
+ */
 int process_interrupts (MSHW* ms)
 {
 	static int icount = 0;
@@ -595,7 +604,7 @@ int process_interrupts (MSHW* ms)
 		if (ms->io[3] & 0x10 && !(ms->interrupt_mask & 0x10))
 		{
 			ms->interrupt_mask |= 0x10;
-			return z80ex_nmi(ms->z80);
+			return z80ex_int(ms->z80);
 		}
 	}
 
@@ -603,7 +612,7 @@ int process_interrupts (MSHW* ms)
 	if (ms->io[3] & 2 && !(ms->interrupt_mask & 2))
 	{
 		ms->interrupt_mask |= 2;
-		return z80ex_nmi(ms->z80);
+		return z80ex_int(ms->z80);
 	}
 
 	// Otherwise ignore this
@@ -888,7 +897,7 @@ int main(int argc, char *argv[])
 			} else if (execute_counter > 15) {
 				execute_counter = 0;
 				while (tstate_counter < interrupt_period ||
-				  !z80ex_nmi_possible(ms.z80)) {
+				  !z80ex_int_possible(ms.z80)) {
 					if (!log_isverbose() && !z80ex_last_op_type(ms.z80)){
 						debug_dasm(&ms);
 					}

commit 165832f08c79c34f051260531c6f286c73da9bd7

msemu: Fix #33, allow power cycling of MS
Commit 04267bb2e13e9832 broke the ability to turn the power off
and back on again of the emulator. The switch from NMI to INT
caused a subtle bug that actually prevented proper power off.

The real hardware, at power off, does a HALT instruction and cuts
power to a large amount of the peripherals. In this emulation
state, NMI is possible but INT is not. This caused an infinite
loop of HALT instructions once the switch to INT was made. This
was resolved by instead checking the state of the instruction to
ensure the Z80 emulation finished executing an instruction and not
just a prefix, rather than checking if INTs are possible.

This may not be a truly accurate emulation at this point, however,
and could warrant further investigation in the future to better
emulate real hardware

src/msemu.c
@@ -897,7 +897,7 @@ int main(int argc, char *argv[])
 			} else if (execute_counter > 15) {
 				execute_counter = 0;
 				while (tstate_counter < interrupt_period ||
-				  !z80ex_int_possible(ms.z80)) {
+				  z80ex_last_op_type(ms.z80)) {
 					if (!log_isverbose() && !z80ex_last_op_type(ms.z80)){
 						debug_dasm(&ms);
 					}