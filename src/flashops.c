02eca454859f6778ecec6b97b7403a00d9a3c578
Kris Bahnsen <Kris@KBEmbedded.com>
2020-10-18 01:07:28 -0700

    flashops: Implemented Dataflash software protect
    This matches the datasheet described behavior, DF defaults now
    to being write protected until the right sequences of read calls
    occur. Dataflash is then protected again once the next sequence
    of read calls occur.
    
    See top of flashops.c for a detailed breakdown of this

--- src/flashops.c
+++ src/flashops.c
@@ -10,6 +10,62 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* Struct to handle software data protection of the 28SF040
+ * The dataflash starts out locked, so in order to get back to the locked
+ * state, the whole array must be traversed. In msemu, a counter can be
+ * used to watch the reads, once the unlock set is matched, then the device
+ * will remain unlocked until successfully matching the lock set.
+ */
+
+/* The dataflash buffer is used to track this state machine. Its a little
+ * clever so here is how it works:
+ *
+ * 512K+1 byte is allocated for the DF, the +1 is used to track the state
+ *   of the lock/unlock pattern, and if the DF is able to be written
+ * The DF starts out in the locked state
+ * In the state machine, only the least significant 13 bits of address
+ *  matter for advancing to the next state. As quoted in the datasheet:
+ *  "Addresses >A12 are 'dont care'"
+ * Each read of DF, the address is compared to the next address in the
+ *   pattern. If the address matches, then move to the next state.
+ * The pacing of this state machine is such that 0x0 through 0x6 are
+ *   the states to unlock. If 0x7 is reached, the DF is unlocked and
+ *   the state machine is advanced to step 0x8 and will await the
+ *   next states to re-lock the DF.
+ * If at any time, there is an address miss or a DF write, the current
+ *   state is ANDed against ~0x7. This has the effect of clearing the state
+ *   machine to either the base locked or unlocked state; that is either
+ *   0x0 or 0x8
+ * The most significant bit of the tracking byte is used to note the state of
+ *   the software data protect and is evaluated if there is an attempt to write
+ *   to the DF
+ *
+ * Tracking byte:
+ * 8'b0xxxxxxx - Dataflash is locked (default state)
+ * 8'b1xxxxxxx - Dataflash is unlocked
+ * bits 6:4 are unused at this time
+ */
+const uint16_t df_unlock_lock_arr[] = {
+	0x1823,
+	0x1820,
+	0x1822,
+	0x0418,
+	0x041B,
+	0x0419,
+	0x041A,
+
+	0x0000,
+
+	0x1823,
+	0x1820,
+	0x1822,
+	0x0418,
+	0x041B,
+	0x0419,
+	0x040A,
+
+	0x0000
+};
 
 /* Helper functions for loading and saving a file to a buffer.
  *
@@ -52,7 +108,9 @@ int df_init(uint8_t **df_buf, ms_opts *options)
 {
 	assert(*df_buf == NULL);
 
-	*df_buf = (uint8_t *)calloc(SZ_512K, sizeof(uint8_t));
+	/* Allocate 512K+1 and store the state of the writeprotect in the +1
+	 * This should not and does not get written back to disk! */
+	*df_buf = (uint8_t *)calloc(SZ_512K+1, sizeof(uint8_t));
 	if (*df_buf == NULL) {
 		printf("Unable to allocate dataflash buffer\n");
 		exit(EXIT_FAILURE);
@@ -96,7 +154,28 @@ int df_deinit(uint8_t **df_buf, ms_opts *options)
 
 uint8_t df_read(uint8_t *df_buf, unsigned int absolute_addr)
 {
-	/* XXX: Add hook here for lock/unlock */
+	volatile uint8_t *wp_track = (df_buf + SZ_512K);
+
+	/* See top of file for explanation of code protect and tracking it */
+
+	/* If the address matches the next number in the sequence, advance
+	 * to the next state. Setting or clearing the write allow bit in
+	 * the tracking byte as needed */
+	if ((absolute_addr & 0x1FFF) == df_unlock_lock_arr[(*wp_track & 0xF)]) {
+		(*wp_track)++;
+		if ((*wp_track & 0xF) == 0x7) {
+			(*wp_track)++;
+			*wp_track |= 0x80; // Set write allowed bit
+		} else if ((*wp_track & 0xF) == 0xF) {
+			/* Clear write allowed bit and return counter to start
+			 * of unlock state machine */
+			*wp_track = 0;
+		}
+	} else {
+		/* If did not match the next state, clear the sequence track */
+		*wp_track &= ~(0x7);
+	}
+
 	return *(df_buf + absolute_addr);
 }
 
@@ -119,6 +198,11 @@ int df_write(uint8_t *df_buf, unsigned int absolute_addr, uint8_t val)
 {
 	static uint8_t cycle;
 	static uint8_t cmd;
+	volatile uint8_t *wp_track = (df_buf + SZ_512K);
+
+	/* ANY write to DF will break the current software protect state
+	 * machine sequence! */
+	*wp_track &= ~(0x7);
 
 	if (!cycle) {
 		switch (val) {
@@ -140,16 +224,28 @@ int df_write(uint8_t *df_buf, unsigned int absolute_addr, uint8_t val)
 		switch(cmd) {
 		  case 0x20: /* Sector erase, execute cmd is 0xD0 */
 			if (val != 0xD0) break;
+			if (!(*wp_track & 0x80)) {
+				log_error(" * DF    Attempted sector erase while DF locked!\n");
+				break;
+			}
 			absolute_addr &= 0xFFFFFF00;
 			log_debug(" * DF    Sector-Erase: 0x%X\n", absolute_addr);
 			memset((df_buf + absolute_addr), 0xFF, 0x100);
 			break;
 		  case 0x10: /* Byte program */
+			if (!(*wp_track & 0x80)) {
+				log_error(" * DF    Attempted byte program while DF locked!\n");
+				break;
+			}
 			log_debug(" * DF    W [%04X] <- %02X\n", absolute_addr,val);
 			*(df_buf + absolute_addr) = val;
 			break;
 		  case 0x30: /* Chip erase, execute cmd is 0x30 */
 			if (val != 0x30) break;
+			if (!(*wp_track & 0x80)) {
+				log_error(" * DF    Attempted chip erase while DF locked!\n");
+				break;
+			}
 			log_debug(" * DF    Chip erase\n");
 			memset(df_buf, 0xFF, SZ_512K);
 			break;
