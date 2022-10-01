de5ce24638f0defb8bacdc31ef70c53826b8d34e
Kris Bahnsen <Kris@KBEmbedded.com>
2022-09-30 21:28:37 -0700

    ms_state: basic savestate
    Unfortunately this is currently dead in the water thanks to z80ex.
    The library doesn't let outside world know the contents of the
    Z80EX_CONTEXT struct so its not readily possibe for us to save
    its state.
    
    Could push a PR to add a snapshot feature. Could integrate z80ex
    in to this project directly and add support needed. Who knows.

--- src/ms_state.c
+++ src/ms_state.c
@@ -1,11 +1,14 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 #include <z80ex/z80ex.h>
 
 #include "msemu.h"
 #include "io.h"
 #include "mem.h"
+#include "lcd.h"
 // TODO: Safe unwinding in case of alloc failure?
 int ms_state_save(ms_ctx *ms)
 {
@@ -15,8 +18,8 @@ int ms_state_save(ms_ctx *ms)
 	 */
 	if (ms->state == NULL) {
 		/* First, allocate state struct */
-		ms->state = (ms_ctx *)malloc(sizeof struct ms_ctx);
-		memset(ms->state, '\0', sizeof(struct ms_ctx);
+		ms->state = (ms_ctx *)malloc(sizeof(struct ms_ctx));
+		memset(ms->state, '\0', sizeof(struct ms_ctx));
 		// XXX: Error checking
 		/* Next, allocate each peripheral buffer via normal init methods */
 		io_init(ms->state);
@@ -45,7 +48,7 @@ int ms_state_save(ms_ctx *ms)
 	cf_copy(ms->state, ms);
 	ram_copy(ms->state, ms);
 	lcd_copy(ms->state, ms);
-	memcpy(ms->state->z80, ms->z80, sizeof(Z80EX_CONTEXT));
+	memcpy(ms->state->z80, ms->z80, sizeof(struct _z80_cpu_context));
 
 	/* Now copy every other struct member. */
 	ms->state->lcd_cas = ms->lcd_cas;
@@ -56,7 +59,52 @@ int ms_state_save(ms_ctx *ms)
 	ms->state->batt_status = ms->batt_status;
 	ms->state->power_button_n = ms->power_button_n;
 
-	/* Ever buffer, hardware state, and Z80 state should be fully copied now */
+	/* Every buffer, hardware state, and Z80 state should be fully copied now */
+
+	return MS_OK;
+}
+
+int ms_state_load(ms_ctx *ms)
+{
+	assert(ms->state != NULL);
+
+	/* Load everything from ms->state, back to ms, leaving state alone */
+	io_copy(ms, ms->state);
+	df_copy(ms, ms->state);
+	cf_copy(ms, ms->state);
+	ram_copy(ms, ms->state);
+	lcd_copy(ms, ms->state);
+	memcpy(ms->z80, ms->state->z80, sizeof(Z80EX_CONTEXT));
+
+	/* Copy every struct member back */
+	ms->lcd_cas = ms->state->lcd_cas;
+	ms->interrupt_mask = ms->state->interrupt_mask;
+	memcpy(ms->key_matrix, ms->state->key_matrix, sizeof(ms->key_matrix));
+	ms->power_state = ms->state->power_state;
+	ms->ac_status = ms->state->ac_status;
+	ms->batt_status = ms->state->batt_status;
+	ms->power_button_n = ms->state->power_button_n;
+
+	return MS_OK;
+}
+
+int ms_state_deinit(ms_ctx *ms)
+{
+	assert(ms->state != NULL);
+
+	/* Free every buffer in ms->state first */
+	io_deinit(ms->state);
+	df_deinit(ms->state, NULL);
+	cf_deinit(ms->state, NULL);
+	ram_deinit(ms->state); /* Frees ram and ram_image */
+	lcd_deinit(ms->state);
+
+	/* Need to manually free z80 context */
+	free(ms->state->z80);
+
+	/* Finally, free state and mark it empty */
+	free(ms->state);
+	ms->state = NULL;
 
 	return MS_OK;
 }
